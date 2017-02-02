58a20ebf3488b9de4f5b22c541bb98ab8996be66
miller2082 <jmillerconcom@gmail.com>
2017-02-02 14:02:54 +0900

    Changes made to dev_blog

--- myBigFatSite/dev_blog/admin.py
+++ myBigFatSite/dev_blog/admin.py
@@ -5,4 +5,3 @@ from .models import BlogPost
 # Register your models here.
 
 admin.site.register(BlogPost)
-
--- myBigFatSite/dev_blog/views.py
+++ myBigFatSite/dev_blog/views.py
@@ -1,10 +1,17 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+from django.utils import timezone
+from .models import BlogPost
+#from django.http import HttpResponse
 
 # Create your views here.
 
-from django.http import HttpResponse
 
+"""
 def index(request):
 	return HttpResponse("You are at James' dev_blog app!!")
+"""
 
+def index(request):
+	posts = BlogPost.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'dev_blog/post_list.html', {'posts': posts})
 
