88a5d62a7fa563d7972527dff8ea0a0433222724
miller2082 <jmillerconcom@gmail.com>
2017-02-17 16:27:11 +0900

    moved css out of pronunciation_training html to separate css file

--- myBigFatSite/pronunciation_training/views.py
+++ myBigFatSite/pronunciation_training/views.py
@@ -1,3 +1,24 @@
+from django.shortcuts import render
+from django.utils import timezone
+#from django.http import HttpResponse
+
+def index(request):
+	def	get(request):
+		TemplateView.as_view(template_name='index.html')
+"""
+
+def index(request):
+	return render(request, 'pronunciation_training/index.html')
+
+"""
+
+
+
+
+
+
+
+
 # from django.shortcuts import render
 #from django.http import HttpResponse
 #from django.views.generic import TemplateView
@@ -5,11 +26,7 @@
 # Create your views here.
 
 
-"""
-def index(request):
-	def	get(request):
-		TemplateView.as_view(template_name='index.html')
-"""
+
 
 
 # From urls, this change to make homepage pronunciation training html and not project homepage:
